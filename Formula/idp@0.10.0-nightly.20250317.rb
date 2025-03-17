b805da376ec0df477c8186540a8eb527d8900c66
cmoulliard <cmoulliard@redhat.com>
2025-03-17 17:03:59 +0100

    Push night release
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

---
+++ Formula/idp@0.10.0-nightly.20250317.rb
@@ -0,0 +1,35 @@
+# Generated through hack/update-taps.sh. Don't edit manually.
+# Next line is used to identify version of the file.
+# idpbuilder_version:0.10.0-nightly.20250317
+#
+require "fileutils"
+
+class IdpAT0100Nightly20250317 < Formula
+  homepage "https://github.com/cnoe-io/idpbuilder"
+
+  v = "v0.10.0-nightly.20250317"
+  version v
+
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/cnoe-io/idpbuilder/releases/download/#{v}/idpbuilder-darwin-amd64"
+    sha256 ""
+  elsif OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/cnoe-io/idpbuilder/releases/download/#{v}/idpbuilder-darwin-arm64"
+    sha256 ""
+  elsif OS.linux? && Hardware::CPU.arm?
+    url "https://github.com/cnoe-io/idpbuilder/releases/download/#{v}/idpbuilder-linux-arm64"
+    sha256 ""
+  else
+    url "https://github.com/cnoe-io/idpbuilder/releases/download/#{v}/idpbuilder-linux-amd64"
+    sha256 ""
+  end
+
+  def install
+    FileUtils.mv("idpbuilder", "idp")
+    bin.install "idp"
+  end
+
+  test do
+    system "#{bin}/idp --version"
+  end
+end

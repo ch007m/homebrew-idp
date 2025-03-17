a683c5f8ad9bbf36e030a91bcb99719d24cc9220
cmoulliard <cmoulliard@redhat.com>
2025-03-17 14:29:51 +0100

    Add idp 0.9.0 formula
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

---
+++ Formula/Idpbuilder@0.9.0.rb
@@ -0,0 +1,43 @@
+# Generated through hack/update-taps.sh. Don't edit manually.
+# Next line is used to identify version of the file.
+# idpbuilder_version:0.9.0
+#
+require "fileutils"
+
+class IdpbuilderAT090 < Formula
+  homepage "https://github.com/cnoe-io/idpbuilder"
+
+  v = "v0.9.0"
+  version v
+
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/cnoe-io/idpbuilder/releases/download/#{v}/idpbuilder-darwin-amd64"
+    sha256 ""
+  elsif OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/cnoe-io/idpbuilder/releases/download/#{v}/idpbuilder-darwin-arm64"
+    sha256 ""
+  elsif OS.linux? && Hardware::CPU.arm?
+    url "https://github.com/cnoe-io/idpbuilder/releases/download/#{v}/idpbuilder-linux-arm64"
+    sha256 ""
+  else
+    url "https://github.com/cnoe-io/idpbuilder/releases/download/#{v}/idpbuilder-linux-amd64"
+    sha256 ""
+  end
+
+  def install
+    if OS.mac? && Hardware::CPU.intel?
+      FileUtils.mv("idpbuilder-darwin-amd64", "idpbuilder")
+    elsif OS.mac? && Hardware::CPU.arm?
+      FileUtils.mv("idpbuilder-darwin-arm64", "idpbuilder")
+    elsif OS.linux? && Hardware::CPU.arm?
+      FileUtils.mv("idpbuilder-linux-arm64", "idpbuilder")
+    else
+      FileUtils.mv("idpbuilder-linux-amd64", "idpbuilder")
+    end
+    bin.install "idpbuilder"
+  end
+
+  test do
+    system "#{bin}/idpbuilder --version"
+  end
+end
--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-# Ch007m Idp
+# Idp
+
+## How to create a new tap repository
+
+```
+brew tap-new ch007m/idp
+Initialized empty Git repository in /opt/homebrew/Library/Taps/ch007m/homebrew-idp/.git/
+[main (root-commit) 743bd21] Create ch007m/idp tap
+ 3 files changed, 97 insertions(+)
+ create mode 100644 .github/workflows/publish.yml
+ create mode 100644 .github/workflows/tests.yml
+ create mode 100644 README.md
+==> Created ch007m/idp
+/opt/homebrew/Library/Taps/ch007m/homebrew-idp
+
+When a pull request making changes to a formula (or formulae) becomes green
+(all checks passed), then you can publish the built bottles.
+To do so, label your PR as `pr-pull` and the workflow will be triggered.
+```
+
+
 
 ## How do I install these formulae?
 
